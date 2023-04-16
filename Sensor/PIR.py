38380f7015571e6b1d44b7b67bb8999bda96b0ac
hepl-leclercq <126668859+hepl-leclercq@users.noreply.github.com>
2023-04-16 19:05:29 +0200

    Update PIR.py

--- Sensor/PIR.py
+++ Sensor/PIR.py
@@ -15,17 +15,7 @@ countdown = 0
 while True:
     # Si un mouvement est détecté
     if motion_pin.value():
-        # Allume la LED
-        led_pin.on()
-        # Démarre le compte à rebours
-        countdown = ON_TIME
-    
-    # Attend pendant un certain temps
-    sleep(SLEEP_TIME)
-    # Décrémente le compte à rebours
-    countdown = countdown - SLEEP_TIME
+        # Affiche un message
+        print("obstacle détecté")
 
-    # Si le compte à rebours est terminé
-    if countdown <= 0:
-        # Éteint la LED
-        led_pin.off()
+   
