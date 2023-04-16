e0ef1af58e0330a146b9543ac2ed2f8200d50196
hepl-leclercq <126668859+hepl-leclercq@users.noreply.github.com>
2023-04-16 18:47:16 +0200

    Create Luminosite.py

---
+++ Sensor/Luminosite.py
@@ -0,0 +1,13 @@
+# Importe le module machine
+import machine
+
+# Crée une instance du port ADC en utilisant l'entrée ADC 1
+luminosite_adc = machine.ADC(1)
+
+# Boucle infinie
+while True:
+    # Lecture de la valeur analogique sur le port ADC
+    lum = luminosite_adc.read_u16()
+
+    # Affichage de la valeur lue
+    print(lum)
